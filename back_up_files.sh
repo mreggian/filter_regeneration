b80ea8947eaecc9c543a1908289eb00f32ba41a3
Marina Reggiani-Guzzo <mreggian@g.syr.edu>
2026-04-28 14:35:41 -0400

    Adding flag --raw

--- back_up_files.sh
+++ back_up_files.sh
@@ -8,8 +8,8 @@ bucket=$(python3 -c "import influxdb_config; print(influxdb_config.BUCKET)")
 export INFLUX_TOKEN=$(python3 -c "import influxdb_config; print(influxdb_config.token)")
 
 echo "Backing up data from PTC10..."
-eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ptc10\")' > daq/backup_ptc_data.txt"
+eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ptc10\")' --raw > daq/backup_ptc_data.csv"
 echo "Backing up data from EZO Humidity sensor..."
-eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ezohum\")' > daq/backup_ezohum_data.txt"
+eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ezohum\")' --raw > daq/backup_ezohum_data.csv"
 echo "Backing up data from EZO Pressure sensor..."
-eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ezoprs\")' > daq/backup_ezoprs_data.txt"
+eval "influx query --org \"${org}\" --token \"${INFLUX_TOKEN}\" 'from(bucket: \"${bucket}\") |> range(start:2026-04-24T17:00:00Z) |> filter(fn: (r) => r._measurement==\"ezoprs\")' --raw > daq/backup_ezoprs_data.csv"
